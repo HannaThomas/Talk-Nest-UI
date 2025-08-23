e48d4a25a46da375eb046690c3a04952aa4b0e51
HannaThomas <manikutty159@gmail.com>
2025-08-23 13:20:47 +0800

    chatRoom message

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -1,5 +1,26 @@
+import { useState } from 'react';
 import './ChatRoom.css'
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:5000');
+
 export default function ChatRoom({ user }) {
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    const sendMessage = () => {
+        if (message.trim()) {
+            const msgData = {
+                sender: user.username,
+                content: message,
+                timestamp: new Date(),
+            };
+            socket.emit('send_message', msgData);
+            setMessages((prev) => [...prev, msgData]);
+            setMessage('');
+        }
+    };
+
     return (
         <div className="container">
             <div className="sidebar">
@@ -10,10 +31,22 @@ export default function ChatRoom({ user }) {
 
             </div>
             <div className="chatArea">
-                <div className="messageFeed"></div>
+                <div className="messageFeed">
+                    <div>
+                        {/* <strong>{msgData.sender}: last message</strong> */}
+                        {messages.map((msg, idx) => (
+                            <div key={idx} className="message">
+                                <strong>{msg.sender}:</strong> {msg.content}
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
                 <div className="inputArea">
-                    <input type="text" placeholder="Type your message..." className="input" />
-                    <button className="button">Send</button>
+                    <input value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        type="text" placeholder="Type your message..." className="input" />
+                    <button onClick={sendMessage} className="button">Send</button>
                 </div>
             </div>
         </div>
